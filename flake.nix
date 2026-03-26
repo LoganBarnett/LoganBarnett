5b0c9f922421a03b3e208df481f61f3fef67bff0
Logan Barnett <logustus@gmail.com>
2026-03-25 21:11:19 -0700

    add flake machinery

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Populate me!";
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/25.11;
+  };
+
+  outputs = { self, nixpkgs }@inputs: let
+    forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    overlays = [];
+    pkgsFor = system: import nixpkgs { inherit overlays system; };
+    packages = (pkgs: let
+
+    in [
+
+    ]);
+  in {
+
+    devShells = forAllSystems (system: {
+      default = (pkgsFor system).mkShell {
+        buildInputs = (packages (pkgsFor system));
+      };
+    });
+  };
+}
